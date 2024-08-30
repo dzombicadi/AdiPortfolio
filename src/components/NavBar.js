94bfff54d206507466f0a7f2e41cfba9c9117ec2
Adi Dzombic <dzombaxd@gmail.com>
2024-08-30 20:01:51 +0200

    Initial commit

---
+++ src/components/NavBar.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "../styles/Navbar.css";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+
+class NavBar extends React.Component {
+  render() {
+    return <h1>LOL</h1>;
+  }
+}
+
+export default NavBar;
